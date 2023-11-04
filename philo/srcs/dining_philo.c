4ce6864f4bb232f8df8bd2d1ca1f768d68888419
Neko-Sato <hisatosi35350@gmail.com>
2023-11-04 22:33:15 +0900

    update

--- philo/srcs/dining_philo.c
+++ philo/srcs/dining_philo.c
@@ -6,15 +6,17 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:22:36 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/11/04 21:31:42 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/11/04 22:31:23 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dining_philo.h"
 #include "philo.h"
+#include "philo_visualizer.h"
 #include "table.h"
 #include "utils.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct s_dining_philo_local
 {
@@ -23,51 +25,76 @@ typedef struct s_dining_philo_local
 	t_philo			**philos;
 }					t_dining_philo_local;
 
+static int			execute(t_dining_philo_local *local,
+						t_dining_philo_args *args);
+static void			final(t_dining_philo_local *local,
+						t_dining_philo_args *args);
+static t_philo		**create_philos(t_dining_philo_local *local,
+						t_dining_philo_args *args);
+
 int	dining_philo(t_dining_philo_args *args)
 {
-	(void)args;
-	return 1;
-	//philovisualizer_init(args->len);
-	// lock_printf = mutex_new();
-	// if (!lock_printf)
-	// 	break ;
-	// philos = create_philos();
-	// if (!philos)
-	// 	break ;
-	// table = table__new(&(t_table_args){args->len, philos});
-	// if (!table)
-	// 	break ;
-	// table__start(table);
-	// table__wait(table);
-	// table__del(&table);
-	// i = 0;
-	// while (i < args->len)
-	// 	philo__del(&philos[i++]);
-	// free(philos);
-	// mutex_del(lock_printf);
-	// return (0);
+	t_dining_philo_local	local;
+	int						ret;
+
+	ret = execute(&local, args);
+	final(&local, args);
+	return (ret);
 }
 
-// static t_philo	**create_philos(t_dining_philo_args *args,
-			// void *lock_printf)
-// {
-// 	t_philo			**philos;
-// 	unsigned int	i;
+static int	execute(t_dining_philo_local *local,
+					t_dining_philo_args *args)
+{
+	philovisualizer_init(args->len);
+	local->lock_printf = mutex_new();
+	if (!local->lock_printf)
+		return (1);
+	local->philos = create_philos(local, args);
+	if (!local->philos)
+		return (1);
+	local->table = table__new(&(t_table_args){args->len, local->philos});
+	if (!local->table)
+		return (1);
+	if (table__start(local->table))
+		return (1);
+	table__wait(local->table);
+	return (0);
+}
 
-// 	philos = malloc(sizeof(t_philo *[args->len]));
-// 	i = 0;
-// 	while (i < args->len)
-// 	{
-// 		philos[i] = philo__new(&(t_philo_args){lock_printf, args->time_to_die,
-// 				args->time_to_eat, args->time_to_sleep, args->must_eat});
-// 		if (!philos[i])
-// 		{
-// 			while (--i)
-// 				free(i);
-// 			free(philos);
-// 			philos = NULL;
-// 			break ;
-// 		}
-// 	}
-// 	return (philos);
-// }
+static void	final(t_dining_philo_local *local,
+					t_dining_philo_args *args)
+{
+	long	i;
+
+	table__del(&local->table);
+	i = 0;
+	while (i < args->len)
+		philo__del(&local->philos[i++]);
+	free(local->philos);
+	mutex_del(local->lock_printf);
+}
+
+static t_philo	**create_philos(t_dining_philo_local *local,
+								t_dining_philo_args *args)
+{
+	t_philo	**philos;
+	long	i;
+
+	philos = malloc(sizeof(t_philo *[args->len]));
+	i = 0;
+	while (philos && i < args->len)
+	{
+		philos[i] = philo__new(&(t_philo_args){local->lock_printf,
+				args->time_to_die, args->time_to_eat, args->time_to_sleep,
+				args->must_eat});
+		if (!philos[i])
+		{
+			while (--i)
+				free(philos[i]);
+			free(philos);
+			philos = NULL;
+		}
+		i++;
+	}
+	return (philos);
+}
